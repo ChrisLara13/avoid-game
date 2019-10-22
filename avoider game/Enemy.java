645a27df48f345950f6a1434e6896c98a0bfb239
Christopher Lara <lara.christopher13@gmail.com>
2019-10-22 10:58:15 -0500

    Enemy Added
    the enemy falls and disappears

---
+++ avoider game/Enemy.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        setLocation( getX(), getY() +1);
+        checkRemove();
+    }
+    
+    private void checkRemove()
+    {
+        World w = getWorld();
+        if(getY() > w.getHeight() + 30)
+        {
+            w.removeObject(this);
+            //despues de eliminar al actor ya no puedes usarlo
+            //porque este ya no existe en el mundo
+        }
+    }
+}
