db7e926a4c43b4b5f3743b0a9c703971097e3d74
Christopher Lara <lara.christopher13@gmail.com>
2019-10-23 15:02:02 -0500

    Screens
    Screens have been added to the game

--- avoider game/AvoiderWorld.java
+++ avoider game/AvoiderWorld.java
@@ -29,4 +29,10 @@ public class AvoiderWorld extends World
             super.addObject(e, x, y);
         }
     }
+    
+    public void endGame()
+    {
+        AvoiderGameOverWorld go = new AvoiderGameOverWorld();
+        Greenfoot.setWorld(go);
+    }
 }
