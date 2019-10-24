5b08a9837cd7ec85b8f24750dc497284fbe7702a
Christopher Lara <lara.christopher13@gmail.com>
2019-10-23 19:06:25 -0500

    Music
    edit the music so it can works well

--- avoider game/AvoiderWorld.java
+++ avoider game/AvoiderWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class AvoiderWorld extends World
 {
+    private GreenfootSound bkgMusic;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,6 +18,10 @@ public class AvoiderWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false); 
+        
+        //initialize the music
+        bkgMusic = new GreenfootSound("sounds/UFO_T-Balt.mp3");
+        bkgMusic.playLoop();
     }
     
     public void act()
